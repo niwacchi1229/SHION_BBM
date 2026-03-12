0c21d1a37cb07e0630a9d1b3dac3728fc627a16e
niwacchi1229 <niwacchi.reo@gmail.com>
2026-03-12 18:46:48 +0900

    マニュアル設定の追加

--- test_cam_02.py
+++ test_cam_02.py
@@ -2,8 +2,11 @@ from picamera2 import Picamera2
 import time
 import os
 
-# 保存用ディレクトリの作成
-os.makedirs("testcaptures", exist_ok=True)
+# パラメーターの設定
+# シャッタースピード：1s = 1,000,000
+SHUTTER_SPEED = 10000
+# ISOの設定：1.0(ISO100),2.0(ISO200)
+ISO_GAIN = 1.0
 
 # 各カメラのインスタンスを作成（コンストラクタにインデックスを渡す）
 # 引数を省略した場合、デフォルト（通常は0）のカメラが選択される
@@ -11,17 +14,19 @@ picam0 = Picamera2(0)
 # picam1 = Picamera2(1)
 
 # それぞれのカメラを個別に設定・開始
-# カメラ0の設定
+# カメラの設定
 # config0 = picam0.create_preview_configuration()でpreviewを使うとクロップされる。stillにするとフル画角で出てくるようになる
 config0 = picam0.create_still_configuration()
-picam0.configure(config0)
-picam0.start()
-
-# カメラ1の設定
-# カメラ0と同様
 # config1 = picam1.create_still_configuration()
+picam0.configure(config0)
 # picam1.configure(config1)
+picam0.start()
 # picam1.start()
+
+# マニュアル設定を適用
+# 露光時間、ゲインを固定する
+picam0.set_controls({"ExposureTime": SHUTTER_SPEED, "AnalogueGain": ISO_GAIN})
+# picam1.set_controls({"ExposureTime": SHUTTER_SPEED, "AnalogueGain": ISO_GAIN})
 
 # カメラを安定させるための時間
 time.sleep(1)
@@ -42,7 +47,7 @@ try:
         print(wait_time)
         
         # ファイル名
-        file0 = f"image_cam0_{i:02d}.jpg"
+        file0 = f"image_cam0_modeM_{i:02d}.jpg"
         # file1 = f"image_cam1_{i:02d}.jpg"
 
         # 撮影
