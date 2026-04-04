92b49adec785ac209e8fe9c9c3a400f752298098
niwacchi1229 <niwacchi.reo@gmail.com>
2026-04-05 01:02:19 +0900

    jpegが定義されていない問題解決

--- test_binary.py
+++ test_binary.py
@@ -1,9 +1,13 @@
 from PIL import Image
+import os
 
-input_file_1 = test_input.jpg
-input_file_2 = input.dng
+# input_file_1 = test_input.jpg
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(script_dir, "test_input.jpg")
 
-with open(f"input_file_1" , "rb") as f :
+# input_file_2 = input.dng
+
+with open(f"{image_path}" , "rb") as f :
     img1 = f.read()
     
 # with open(f"input_file_2" , "rb") as f :
